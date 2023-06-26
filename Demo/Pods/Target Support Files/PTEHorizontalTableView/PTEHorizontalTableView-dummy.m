ac255361d49e64915ea4e34bb30426ac8ef33377
removedporn <86824510+removedporn@users.noreply.github.com>
2023-06-26 23:08:23 +0800

    Delete Demo directory

--- Demo/Pods/Target Support Files/PTEHorizontalTableView/PTEHorizontalTableView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PTEHorizontalTableView : NSObject
-@end
-@implementation PodsDummy_PTEHorizontalTableView
-@end
--- Demo/Pods/Target Support Files/Pods-HorizontalTable/Pods-HorizontalTable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HorizontalTable : NSObject
-@end
-@implementation PodsDummy_Pods_HorizontalTable
-@end
